d44470855f9d4e6bf8c59d872334b9c8de6e7e3c
jimenez2society <121840097+jimenez2society@users.noreply.github.com>
2023-03-16 09:47:26 -0600

    added the actual functionality of the ToTheTopButton to scroll to the top of the page when clicked

--- src/components/TotheTopButton/ToTheTopButton.js
+++ src/components/TotheTopButton/ToTheTopButton.js
@@ -2,8 +2,11 @@ import React from "react";
 import { right } from "../../assets/svg/svg";
 import "./ToTheTopButton.css";
 function ToTheTopButton() {
+  const handleScrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
   return (
-    <div className="ToTheTopButton">
+    <div onClick={handleScrollToTop} className="ToTheTopButton">
       <div className="arrow">{right}</div>
     </div>
   );
